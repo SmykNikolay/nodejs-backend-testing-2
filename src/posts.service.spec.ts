33526f372e4fdcaff880f3f775c02fe0a5db2814
SmykNikolay <nikolaysmyk613@gmail.com>
2024-05-15 12:48:16 +0300

    test

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -7,26 +7,36 @@ describe('PostsService', () => {
     postsService = new PostsService();
   });
 
-  describe('.findMany', () => {
-    const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
-    ];
+describe('.findMany', () => {
+  const posts = [
+    {id: '1', text: 'Post 1'},
+    {id: '2', text: 'Post 2'},
+    {id: '3', text: 'Post 3'},
+    {id: '4', text: 'Post 4'},
+  ];
 
-    beforeEach(() => {
-      posts.forEach((post) => postsService.create(post));
-    });
+  beforeEach(() => {
+    posts.forEach((post) => postsService.create(post));
+  });
 
-    it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
-    });
+  it('should return all posts if called without options', () => {
+    const foundPosts = postsService.findMany();
+    expect(foundPosts.length).toEqual(posts.length);
+    expect(foundPosts).toEqual(posts);
+  });
 
-    it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
-    });
+  it('should return correct posts for skip and limit options', () => {
+    const foundPosts = postsService.findMany({skip: 1, limit: 2});
+    expect(foundPosts.length).toEqual(2);
+    expect(foundPosts).toEqual(posts.slice(1, 3));
+  });
 
-    // реализуйте недостающие тест-кейсы
+  it('should return all posts from skip if limit is not provided', () => {
+    const foundPosts = postsService.findMany({skip: 2});
+    expect(foundPosts.length).toEqual(2);
+    expect(foundPosts).toEqual(posts.slice(2));
   });
 });
+
+  
+  });
